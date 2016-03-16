607f9e480a6fcd983aebdd0f0e513bec78d98dfa
Carlos Sá <carlossa.dev@gmail.com>
2016-03-15 21:40:40 -0300

    Criando o model de edicoes do programa.

---
+++ bbb/editions/models.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+from django.db import models
+
+
+class Editions(models.Model):
+    edition = models.IntegerField(null=False, blank=False)
+    date_start = models.DateTimeField(u'Data de início', auto_now=False,
+                                      auto_now_add=False)
+    date_finish = models.DateTimeField(u'Data de término', auto_now=False,
+                                       auto_now_add=False)
+    status = models.BooleanField(default=True)
+
+    def __unicode__(self):
+        return u'%s' % self.edition
+
+    class Meta:
+        verbose_name = u'Edição'
+        verbose_name_plural = u'Edições'
