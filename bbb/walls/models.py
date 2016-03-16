b71f4c53d5dd1e839a674b18070dc0ae3c301072
Carlos Sá <carlossa.dev@gmail.com>
2016-03-16 13:57:24 -0300

    Incluindo os campos date_start e participants para que possa ter mais de 2 integrantes no paredao.

--- bbb/walls/models.py
+++ bbb/walls/models.py
@@ -6,17 +6,17 @@ from bbb.participants.models import Participants
 
 class Walls(models.Model):
     edition = models.ForeignKey(Editions, null=False, blank=False)
-    participant1 = models.ForeignKey(Participants, null=False, blank=False,
-                                     related_name='participant1')
-    participant2 = models.ForeignKey(Participants, null=False, blank=False,
-                                     related_name='participant2')
+    participants = models.ManyToManyField(Participants)
+    date_start = models.DateTimeField(u'Data do início', auto_now_add=True)
     date_finish = models.DateTimeField(u'Data de término', auto_now=False,
                                        auto_now_add=False)
     status = models.BooleanField(default=True)
 
     def __unicode__(self):
-        return u'Participantes: %s vs %s' % (self.participant1.name,
-                                             self.participant2.name)
+        participants = []
+        for participant in self.participants.all():
+            participants.append(participant.name)
+        return u'Participantes: %s' % ','.join(participants)
 
     class Meta:
         verbose_name = u'Paredão'
