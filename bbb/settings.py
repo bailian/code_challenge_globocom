ec47d57b447f427cb6616cd1b53e89462b5a8512
Carlos Sá <carlossa.dev@gmail.com>
2016-03-17 00:08:10 -0300

    Configurando o atributo dirs do template.

--- bbb/settings.py
+++ bbb/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'bbb.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'bbb/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
