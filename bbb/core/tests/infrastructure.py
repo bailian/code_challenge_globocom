e7d08f0a5d9f71f334a67ebde3afefb6c0bbd270
Carlos Sá <carlossa.dev@gmail.com>
2016-03-17 13:18:42 -0300

    Criando participantes para os testes.

--- bbb/core/tests/infrastructure.py
+++ bbb/core/tests/infrastructure.py
@@ -1,5 +1,15 @@
 # coding: utf-8
 from django.test import TestCase
+from StringIO import StringIO
+from PIL import Image
+from django.core.files.base import File
+from bbb.participants.models import Participants
+
+
+def __create_participant__(name, avatar, codename=None, description=None,
+                           status=True):
+    return Participants.objects.create(name=name, codename=codename,
+                                       image=avatar, status=status)
 
 
 class TestCaseInfrastructure(TestCase):
@@ -7,5 +17,23 @@ class TestCaseInfrastructure(TestCase):
     def setUpClass(cls):
         super(TestCaseInfrastructure, cls).setUpClass()
 
+    @staticmethod
+    def get_image_file(name='test.png', ext='png', size=(50, 50),
+                       color=(256, 0, 0)):
+        file_obj = StringIO()
+        image = Image.new("RGBA", size=size, color=color)
+        image.save(file_obj, ext)
+        file_obj.seek(0)
+        return File(file_obj, name=name)
+
     def setUp(self):
-        pass
+        self.participants = []
+
+        self.participants.append(__create_participant__(
+            'Participante 1', self.get_image_file()))
+        self.participants.append(__create_participant__(
+            'Participante 2', self.get_image_file()))
+
+    def test_vai(self):
+        participant = self.participants[0]
+        print participant.name
