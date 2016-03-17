b1f38fb75a94666cb02e44a3765fc15052d08e25
Carlos Sá <carlossa.dev@gmail.com>
2016-03-17 15:19:10 -0300

    Criando test para o metodo que retorna a lista de participantes do paredao.

---
+++ bbb/walls/tests/test_models.py
@@ -0,0 +1,12 @@
+#!/bbb/bin/python
+# coding: utf-8
+from bbb.core.tests.infrastructure import TestCaseInfrastructure
+
+
+class TestWalls(TestCaseInfrastructure):
+    def setUp(self):
+        super(TestWalls, self).setUp()
+
+    def test_get_participants_of_wall(self):
+        participants = self.wall.get_participants()
+        self.assertEqual(self.participants, participants)
