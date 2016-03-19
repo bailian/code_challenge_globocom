da976eca84e65c02bc2937e2f816c290df78c7b6
Carlos Sá <carlossa.dev@gmail.com>
2016-03-19 20:47:17 -0300

    Inicializando o valor o campo paredao com o id do mesmo.

--- bbb/voting/forms.py
+++ bbb/voting/forms.py
@@ -5,6 +5,10 @@ from bbb.voting.models import Voting
 
 
 class VotingForm(forms.Form):
+    def __init__(self, wall, *args, **kwargs):
+        super(VotingForm, self).__init__(*args, **kwargs)
+        self.fields['wall'].initial = wall.pk
+
     wall = forms.CharField(widget=forms.HiddenInput())
     vote = forms.CharField(widget=forms.HiddenInput())
 
