db8809f8cd1a46640b3d7f007411309f66340146
Carlos Sá <carlossa.dev@gmail.com>
2016-03-22 11:08:47 -0300

    Finalizando a view do relatorio de votos por hora.

--- bbb/walls/views.py
+++ bbb/walls/views.py
@@ -2,6 +2,7 @@
 # coding: utf-8
 from django.shortcuts import render
 import logging
+from datetime import datetime
 from django.db.models import Count
 from bbb.walls.models import Walls
 
@@ -67,24 +68,59 @@ def get_all_votes_per_hours(request):
     msg = None
     result_walls = {'walls': []}
 
-    walls = Walls.objects.filter(status=True)
+    walls = Walls.objects.filter(edition__status=True)
     for wall in walls:
-        # vote_per_hours = wall.voting_set.extra({
-        #     'date': "EXTRACT(\'day\' FROM \"date_vote\")",
-        #     'hour': "EXTRACT(\'hour\' FROM \"date_vote\")"
-        # }).values('date', 'hour').order_by('date', 'hour').annotate(Count('id'))
-        vote_per_hours = wall.voting_set.extra({
-            'date': "DATEPART(\'day\', \"date_vote\")",
-            'hour': "DATEPART(\'hour\', \"date_vote\")"
-        }).values('date', 'hour').order_by('date', 'hour').annotate(Count('id'))
-        print vote_per_hours
+        vote_per_date = wall.voting_set.all().order_by('date_vote')
+        day = None
+        for vote_date in vote_per_date:
+            if vote_date.date_vote.strftime('%Y-%m-%d') != day:
+                for hour_num in range(0, 23):
+                    day_hour_start = datetime(
+                        vote_date.date_vote.year, vote_date.date_vote.month,
+                        vote_date.date_vote.day, hour_num, 00, 00,
+                         480538,
+                    )
+                    day_hour_finish = datetime(
+                        vote_date.date_vote.year, vote_date.date_vote.month,
+                        vote_date.date_vote.day, hour_num, 59, 00,
+                        480538,
+                    )
+
+                    all_day = vote_per_date.filter(
+                        date_vote__gte=day_hour_start,
+                        date_vote__lte=day_hour_finish
+                    )
+                    if len(all_day) > 0:
+                        dates = []
+                        str_hour = hour_num
+                        if hour_num < 10:
+                            str_hour = '0%s' % hour_num
+                        hour = {
+                            'date':
+                                vote_date.date_vote.strftime('%d/%m/%Y'),
+                            'hour': '%s:00' % str_hour,
+                            'participants': []
+                        }
+
+                        for participant in wall.participants.all():
+                            hour['participants'].append(
+                                {'name': participant.name,
+                                 'votes':
+                                     all_day.filter(vote=participant).count()
+                                 })
+                        dates.append(hour)
+
+                        wall_dict = {
+                            'wall': wall,
+                            'dates': dates
+                        }
+                        result_walls['walls'].append(wall_dict)
+                day = vote_date.date_vote.strftime('%Y-%m-%d')
 
     context = {
-        'subeditors': 'Votos do participante nos paredões por hora',
-        # 'participant': participant,
-        # 'total_votes': total_votes,
-        # 'wall': wall,
-        'type_report': 'all_votes_participant',
+        'subeditors': 'Votos dos participantes nos paredões por hora',
+        'walls': result_walls,
+        'type_report': 'all_votes_participant_per_hour',
         'msg': msg,
     }
     return render(request, 'desktop/report.html', context)
